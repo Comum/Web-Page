bae634a75473266875367e674c87df2da0601a7b
Comum <miguel.rib.20@gmail.com>
2017-04-29 23:31:11 +0100

    Started adding content load functionally
    Has some problems changing from content to another.

--- js/menu_handler.js
+++ js/menu_handler.js
@@ -1,32 +1,102 @@
+function initiate_svg() {
+  $(".js-second-nav-wrapper").empty();
+  $(".js-second-nav-wrapper").addClass('js-initiate-sequence');
+  //criar svg
+};
+
+function about_button_clicked(id) {
+
+  if(id === 'about') {
+    return true;
+  }
+  if((id.split('_').length > 1) && (id.split('_')[1] === 'about')){
+    return true;
+  }
+  return false;
+};
+
+function entry_buttons_pressed(id) {
+  if(id.split('_').length === 1) {
+    return true;
+  }
+};
+
+function container_fade_in(container) {
+  $("." + container).fadeTo("slow", 1);
+};
+
+function container_fade_out(container) {
+  $("." + container).fadeTo("slow", 0);
+}
+
+function entry_menu_to_second() {
+  $('.js-main-container').animate({
+      'margin-top': '-' + $(".js-main-container").height() + 'px'
+  }, 1250, function() {
+    //menu do segundo menu -> fade in
+    container_fade_in("js-second-nav-wrapper");
+    container_fade_in("js-content-container");
+  });
+};
+
+function clear_content_area() {
+  $(".js-content-area").empty();
+};
+
+function load_new_content(content) {
+  $.get("content/" + content + ".html", function(data){
+    $(".js-content-area").html(data);
+  });
+}
+
 $(document).ready(function() {
 
-  $(".js-main-container").on('click', '.js-menu-option', function() {
+  var id_elem;
 
-    if($(this).attr('id') === 'about') {
-      $(".js-second-nav-wrapper").empty();
-      //criar svg
+  $(".js-main-view").on('click', '.js-menu-option', function() {
+
+    id_elem = $(this).attr('id');
+
+    if(about_button_clicked(id_elem)) {
+      initiate_svg();
+    }
+    else {
+      clear_content_area();
+      if(id_elem.split('_').length > 1) {
+        load_new_content(id_elem.split('_')[1]);
+        container_fade_in("js-content-container");
+      }
+      else {
+        load_new_content(id_elem);
+      }
     }
 
-    $('.js-main-container').animate({
-        'margin-top': '-' + $(".js-main-container").height() + 'px'
-    }, 1250, function() {
-
-      //menu do segundo menu -> fade in
-      $(".js-second-nav-wrapper").fadeTo("slow", 1);
-    });
+    if(entry_buttons_pressed(id_elem)) {
+      entry_menu_to_second();
+    }
   });
 
   $("body").on('click', '.js-second-nome-reload', function() {
     /*
      * ao clickar no segundo menu manda opacity a 0 e depois recarrega novo conteudo
      */
-    $(".js-second-nav-wrapper").fadeTo("slow", 0);
+    container_fade_out("js-second-nav-wrapper");
+    container_fade_out("js-content-area");
 
-    if($(this).attr('id') === 'second_nome_reload') {
+    if($(this).attr('id') === 'second_nome_reload') { //click no nome
 
       $('.js-main-container').animate({
         'margin-top': '0px'
       }, 1250);
+    }
+  });
+
+  $(document).keyup(function(e) {
+     if(e.keyCode == 27) { // 'ESC' pressionado
+        if($(".js-second-nav-wrapper").hasClass("js-initiate-sequence")) {
+          //reset a tudo
+          location.reload();
+        }
     }
   });
 });
